1f30381caa5ec7f760c790402096f4b8d9251697
mgalarowicz <rezi610@gmail.com>
2020-10-21 18:27:24 +0200

    10.2.ERROR_HANDLING.VALIDATION - ErrorMiddleware and Client-side error handling

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -19,10 +19,13 @@ const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({match, hi
   const {activity, loadActivity, loadingInitial} = activityStore;
 
   useEffect(() => {
-    loadActivity(match.params.id)
-  }, [loadActivity, match.params.id])
+    loadActivity(match.params.id);
+  }, [loadActivity, match.params.id, history])
 
-  if (loadingInitial || !activity) return <LoadingComponent content='Loading activity...' />
+  if (loadingInitial) return <LoadingComponent content='Loading activity...' />
+
+  if (!activity)
+    return <h1>Not found</h1>
 
   return (
     <Grid>
