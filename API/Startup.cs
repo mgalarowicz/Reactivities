820b3e7270e5b8019224929f17f7d4d95b9b21c6
mgalarowicz <rezi610@gmail.com>
2020-10-27 18:16:15 +0100

    12.2.ASPNETCORE.IDENTITY - BaseController, Infrastructure project, JWT

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,7 +1,9 @@
 using API.Middleware;
 using Application.Activities;
+using Application.Interfaces;
 using Domain;
 using FluentValidation.AspNetCore;
+using Infrastructure.Security;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -47,6 +49,8 @@ namespace API
             var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
             identityBuilder.AddEntityFrameworkStores<DataContext>();
             identityBuilder.AddSignInManager<SignInManager<AppUser>>();
+
+            services.AddScoped<IJwtGenerator, JwtGenerator>();
 
             services.AddAuthentication();
         }
